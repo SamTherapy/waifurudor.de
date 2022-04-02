79d8c9f74ec5cb6e5d84b7ff396c7db4aa3f30b3
grumbulon <grumbulon@dismail.de>
2022-04-02 19:57:42 -0400

    Working api call - it only took an hour hehe haha

---
+++ src/helpers/API.js
@@ -0,0 +1,28 @@
+const request = require("request");
+const parseConfig = require("./config")
+
+function search() {
+    let configFile = "./config.json";
+    parseConfig.readConfig(configFile, (err, config)=>{
+        if (err) {
+            console.log(err);
+            return;
+        }
+        request(
+            `https://${config.booru}.donmai.us/posts/6.json`,
+            { json: true },
+            (err, res, body) => {
+              if (err) {
+                return console.log(err);
+              }
+        
+              console.log(body.id);
+              console.log(body.created_at);
+              console.log(body.rating);
+              console.log(body.large_file_url);
+            }
+          );
+    });
+}
+
+module.exports = { search };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-const request = require('request');
+const search = require("./helpers/API")
 
+async function main() {
+  search.search();
+}
 
-request('https://testbooru.donmai.us/posts/6.json', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  console.log(body.id);
-  console.log(body.created_at);
-  console.log(body.rating);
-  console.log(body.large_file_url)
-});
+main();
