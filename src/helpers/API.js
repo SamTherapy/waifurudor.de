8acc2a04bf95dc3b05f54b54e6eabf9f36c8a7a0
grumbulon <grumbulon@dismail.de>
2022-04-02 21:39:29 -0400

    Added stuff so you can search safe or not, and excluded images from being uploaded to git

--- src/helpers/API.js
+++ src/helpers/API.js
@@ -11,21 +11,31 @@ function search() {
       console.log(err);
       return;
     }
-    //Get list of posts for the tag(s)
-    request(
-      `https://${config.booru}.donmai.us/posts.json?tags=${config.tags}&z=1`,
-      { json: true },
-      (err, res, body) => {
+
+    if (config.rating === "safe") {
+      let queryUrl = `https://${config.booru}.donmai.us/posts.json?tags=${config.tags}+rating:${config.rating}&z=1`;
+      request(queryUrl, { json: true }, (err, res, body) => {
         if (err) {
           return console.log(err);
         }
         const randPost = Math.floor(Math.random() * body.length);
         const postID = body[randPost].id;
         getPost(postID);
-      }
-    );
+      });
+    } else {
+      let queryUrl = `https://${config.booru}.donmai.us/posts.json?tags=${config.tags}&z=1`;
+      request(queryUrl, { json: true }, (err, res, body) => {
+        if (err) {
+          return console.log(err);
+        }
+        const randPost = Math.floor(Math.random() * body.length);
+        const postID = body[randPost].id;
+        getPost(postID);
+      });
+    }
   });
 }
+
 function getPost(postID) {
   parseConfig.readConfig(configFile, (err, config) => {
     if (err) {
